50b3bcb9b6e38723f1617c3e5336c9ec22ab4400
vustkdgus <vustkdgus@naver.com>
2021-03-16 17:27:49 +0900

    21.03.16 커밋

---
+++ WinformApp/WinFormAdvancedBank/BookRentalShopApp/Helper/Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalShopApp.Helper
+{
+    public static class Common
+    {
+        public static string ConnString = "Data Source = 127.0.0.1; Initial Catalog = IoTData;" +
+            " Persist Security Info=True;User ID = sa; Password = mssql_p @ssw0rd!;";
+
+        public static string LoginUserId = string.Empty;
+    }
+}
