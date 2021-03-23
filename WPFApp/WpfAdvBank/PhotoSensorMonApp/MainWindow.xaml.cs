882837fdf970728dabd52144efc04ac7123a0fa0
vustkdgus <vustkdgus@naver.com>
2021-03-23 16:54:44 +0900

    21.03.23 커밋

---
+++ WPFApp/WpfAdvBank/PhotoSensorMonApp/MainWindow.xaml.cs
@@ -0,0 +1,181 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using MahApps.Metro.Controls;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace PhotoSensorMonApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public class SensorData
+        {
+            public int Idx { get; set; }
+            public DateTime CurrentDt { get; set; }
+            public int Value { get; set; }
+            public Boolean SimulFlag { get; set; }
+
+            public SensorData(int idx, DateTime currentDt, int value, bool simulFlag)
+            {
+                Idx = idx;
+                CurrentDt = currentDt;
+                Value = value;
+                SimulFlag = simulFlag;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            logger.Info("PhotoSensorMonApp Initialized...");
+        }
+
+        public ChartValues<int> ChartValues { get; set; }
+
+        public int SensorValue { get; set; }
+
+        public DispatcherTimer CustomTimer { get; set; }
+
+        private string connString = "Data Source=210.119.12.214;" +
+            "Initial Catalog=IoTData;" +
+            "Persist Security Info=True;" +
+            "User ID=sa;Password = mssql_p@ssw0rd!";
+
+        public SensorData GetRealTimeSenseor()
+        {
+            SensorData result = null;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+
+                    var query = @"SELECT TOP 1 Idx
+                              ,CurrentDt
+                              ,Value
+                              ,SimulFlag
+                          FROM Tbl_PhotoResistor
+                         ORDER BY Idx DESC ";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    var temp = cmd.ExecuteReader();
+                    if (temp.Read())
+                    {
+                        result = new SensorData(Convert.ToInt32(temp["Idx"]), 
+                                                Convert.ToDateTime(temp["CurrentDt"]),
+                                                Convert.ToInt32(temp["Value"]),
+                                                Convert.ToBoolean(temp["SimulFlag"]));
+                    }
+                }
+
+                logger.Info("GetRealTimeSenseor() completed.");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"예외발생 : {ex.Message}");
+                logger.Error($"GetRealTimeSenseor() error occured : {ex.Message}");
+            }
+
+            return result;
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            //var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
+            //var y = x.Select(v => Math.Abs(v) < 1e-10 ? 1 : Math.Sin(v) / v).ToArray();
+            //ChtLine.Plot(x, y);
+            ChartValues = new ChartValues<int>(); // { 10, 4, 6, 5, 3, 5, 1, 2, 3, 20};
+            GrdHistory.DataContext = ChartValues;
+
+            CustomTimer = new DispatcherTimer();
+            CustomTimer.Interval = TimeSpan.FromSeconds(1);
+            CustomTimer.Tick += CustomTimer_Tick;
+            // CustomTimer.Start();
+
+        }
+
+        private void CustomTimer_Tick(object state, EventArgs e)
+        {
+            SensorValue = GetRealTimeSenseor().Value;
+            GrdRealTime.DataContext = SensorValue;
+        }
+
+        private void MnuStart_Click(object sender, RoutedEventArgs e)
+        {
+            CustomTimer.Start();
+        }
+
+        private void MnuStop_Click(object sender, RoutedEventArgs e)
+        {
+            CustomTimer.Stop();
+        }
+
+        private void MnuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MnuLoad_Click(object sender, RoutedEventArgs e)
+        {
+            HistoryValues.ItemsSource = GetHistorySensors();
+        }
+
+        private List<DataPoint> GetHistorySensors()
+        {
+            List<DataPoint> result = new List<DataPoint>();
+
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(connString))
+                {
+                    if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+
+                    var query = $@"SELECT Idx, Value 
+                                  FROM  Tbl_PhotoResistor 
+                                 WHERE CurrentDt > CONVERT(DATETIME, '{DateTime.Now.ToString("yyyy-MM-dd")}')
+                                 ORDER BY Idx";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    
+                    while (reader.Read())
+                    {
+                        result.Add(new DataPoint(Convert.ToInt32(reader[0]), Convert.ToInt32(reader[1])));
+                    }
+                }
+                logger.Info("GetHistorySensors() completed.");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"예외발생 : {ex.Message}");
+                logger.Error($"GetHistorySensors() error occured : {ex.Message}");
+            }
+
+            return result;
+        }
+
+       
+    }
+}
