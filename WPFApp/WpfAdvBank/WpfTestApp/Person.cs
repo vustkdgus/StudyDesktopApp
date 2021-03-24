2ad53ae0f634c2425f16e9c3058d3834fe5fcd39
vustkdgus <vustkdgus@naver.com>
2021-03-24 14:09:27 +0900

    멀티바인딩

---
+++ WPFApp/WpfAdvBank/WpfTestApp/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace WpfTestApp
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
